863280f42c0d70a62e63bc8bba916f7fc41de8ae
Boris Kolpackov <boris@codesynthesis.com>
2019-04-19 16:03:56 +0200

    Pass original header name and inclusion style in mappers INCLUDE command
    TODO:
      - Update mapper documentation.
      - Update GCC's build-in mapper implementation?

--- gcc/cp/module.cc
+++ gcc/cp/module.cc
@@ -3746,7 +3746,7 @@ public:
   {
     return get_response (state->from_loc) > 0 ? cmi_response (state) : NULL;
   }
-  bool translate_include (location_t, const char *);
+  bool translate_include (location_t, const char *, bool, const char *);
 
 public:
   /* After a response that may be corked, eat blank lines until it is
@@ -12461,14 +12461,18 @@ module_mapper::export_done (const module_state *state)
    buffer containing the translation text (ending in two \n's).  */
 
 bool
-module_mapper::translate_include (location_t loc, const char *path)
+module_mapper::translate_include (location_t loc,
+                                  const char *fname, bool angle,
+                                  const char *path)
 {
   bool xlate = false;
 
   timevar_start (TV_MODULE_MAPPER);
   if (mapper->is_server ())
     {
-      send_command (loc, "INCLUDE %s", path);
+      send_command (loc, "INCLUDE %c%s%c %s",
+                    angle ? '<' : '"', fname, angle ? '>' : '"',
+                    path);
       if (get_response (loc) <= 0)
 	return false;
 
@@ -17232,7 +17236,7 @@ module_map_header (cpp_reader *reader, location_t loc, bool search,
 
 bool
 module_translate_include (cpp_reader *reader, line_maps *lmaps, location_t loc,
-			  const char *path)
+			  const char *name, bool angle, const char *path)
 {
   if (!modules_p ())
     {
@@ -17254,7 +17258,7 @@ module_translate_include (cpp_reader *reader, line_maps *lmaps, location_t loc,
     {
       size_t len = strlen (path);
       path = canonicalize_header_name (NULL, loc, true, path, len);
-      res = mapper->translate_include (loc, path);
+      res = mapper->translate_include (loc, name, angle, path);
     }
 
   dump (dumper::MAPPER) && dump (res ? "Translating include to import"
