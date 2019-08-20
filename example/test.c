3d2815b07cd48ca9f86895f76a4eaecfc8dcc7f8
Holger Lamm <Holger.Lamm@continental-corporation.com>
2019-08-20 09:15:10 +0200

    Provide lcov and testfunction

---
+++ example/test.c
@@ -0,0 +1,16 @@
+#include "test.h"
+
+
+int testfunc (int var_a, int var_b)
+{
+   int result_x;
+   if (var_a && var_b)
+   {
+      result_x = 0;
+   }
+   else
+   {
+      result_x = 1;
+   }
+   return result_x;
+}
