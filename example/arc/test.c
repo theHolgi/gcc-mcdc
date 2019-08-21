6ec6da6879b3bbe2c927bef90fb54c9b7cca9091
Holger Lamm <Holger.Lamm@continental-corporation.com>
2019-08-21 10:20:23 +0200

    profiling && and ||

--- example/arc/test.c
+++ example/arc/test.c
@@ -1,4 +1,4 @@
-#include "test.h"
+#include <test.h>
 
 
 int testfunc (int var_a, int var_b)
@@ -13,18 +13,4 @@ int testfunc (int var_a, int var_b)
       result_x = 1;
    }
    return result_x;
-}
-
-int testfunc2 (int var_a, int var_b)
-{
-   int x;
-   if (var_a || var_b)
-   {
-      x = 0;
-   }
-   else
-   {
-      x = 1;
-   }
-   return x;
 }
--- example/mcdc/test.c
+++ example/mcdc/test.c
@@ -14,17 +14,3 @@ int testfunc (int var_a, int var_b)
    }
    return result_x;
 }
-
-int testfunc2 (int var_a, int var_b)
-{
-   int x;
-   if (var_a || var_b)
-   {
-      x = 0;
-   }
-   else
-   {
-      x = 1;
-   }
-   return x;
-}
